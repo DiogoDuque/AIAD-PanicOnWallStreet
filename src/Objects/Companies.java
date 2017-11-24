4e3f91416630dc1302042ebb53607c1766c0418d
Bruno <pandasarehere@gmail.com>
2017-11-24 07:19:55 +0000

    work in progress

---
+++ src/Objects/Companies.java
@@ -0,0 +1,16 @@
+package objects;
+
+java.util.list;
+
+public class Companies {
+
+
+    private String name;
+
+    private int numShares;
+    private double compValue;
+
+    public String status;
+
+    private List<Shares> shares;
+}
