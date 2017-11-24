0fb18e41b8e9669438594abca05da8b60ad1654a
Diogo Duque <diogoduque13@hotmail.com>
2017-11-24 17:35:50 +0000

    "handshake" example completed

--- src/main/java/agent/InvestorAgent.java
+++ src/main/java/agent/InvestorAgent.java
@@ -1,16 +1,17 @@
 package agent;
 
+import com.google.gson.Gson;
+import communication.Message;
 import jadex.bridge.IInternalAccess;
 import jadex.bridge.service.RequiredServiceInfo;
 import jadex.bridge.service.component.IRequiredServicesFeature;
 import jadex.bridge.service.search.SServiceProvider;
-import jadex.commons.future.DefaultResultListener;
 import jadex.commons.future.IFuture;
 import jadex.commons.future.ISubscriptionIntermediateFuture;
 import jadex.commons.future.IntermediateDefaultResultListener;
 import jadex.micro.annotation.*;
-import service.ComsService;
-import service.IComsService;
+import communication.ComsService;
+import communication.IComsService;
 
 @RequiredServices({
         @RequiredService(name="coms", type= IComsService.class, binding=@Binding(scope= RequiredServiceInfo.SCOPE_PLATFORM))
@@ -30,24 +31,36 @@ public class InvestorAgent
 	protected IComsService coms;
 
 	@AgentBody
-	public IFuture<Void> executeBody()
+	public void executeBody()
 	{
+        String myCid = agent.getComponentIdentifier().getName();
 		this.coms = (IComsService)reqServ.getRequiredService("coms").get();
         IComsService iComs = SServiceProvider.getService(agent,IComsService.class, RequiredServiceInfo.SCOPE_PLATFORM).get();
         ISubscriptionIntermediateFuture<String> sub = iComs.subscribeComs();
         sub.addIntermediateResultListener(new IntermediateDefaultResultListener<String>() {
             @Override
             public void intermediateResultAvailable(String result) {
-                //super.intermediateResultAvailable(result);
-                log(result);
+                Message msg = new Gson().fromJson(result, Message.class);
+
+                if(msg.getSenderCid().equals(myCid))
+                    log("ignoring my own message...");
+                else if(msg.getReceiverCid()==null) { //broadcasted
+                    log(msg.getMsg());
+                    coms.broadcast(new Message(myCid, msg.getSenderCid(), "hi back ;) ").toJsonStr());
+                } else if(msg.getReceiverCid().equals(myCid)) //broadcasted, but for me
+                    log(msg.getMsg(),"from "+msg.getSenderCid()+" just for me *.*");
+                else log("ignoring msg not for me..."); //broadcasted, but not for me
             }
         });
 
-        SServiceProvider.getService(agent,IComsService.class, RequiredServiceInfo.SCOPE_PLATFORM).get().broadcast("Investor says hi");
-		return IFuture.DONE;
+        coms.broadcast(new Message(agent.getComponentIdentifier().getName(),"I Hello").toJsonStr());
 	}
 
 	private void log(String msg){
-	    System.out.println(agent.getComponentIdentifier().getName()+" -> "+msg);
+	    System.out.println(agent.getComponentIdentifier().getName()+": "+msg);
+    }
+
+    private void log(String msg, String extra){
+        System.out.println(agent.getComponentIdentifier().getName()+": "+msg+"; "+extra);
     }
 }
--- src/main/java/communication/ComsService.java
+++ src/main/java/communication/ComsService.java
@@ -1,4 +1,4 @@
-package service;
+package communication;
 
 import jadex.bridge.IInternalAccess;
 import jadex.bridge.service.annotation.Service;
@@ -6,12 +6,15 @@ import jadex.bridge.service.annotation.ServiceComponent;
 import jadex.commons.future.ISubscriptionIntermediateFuture;
 import jadex.commons.future.SubscriptionIntermediateFuture;
 
+import java.util.ArrayList;
+
 @Service
 public class ComsService implements IComsService {
 
     @ServiceComponent
     private IInternalAccess agent;
 
+    private ArrayList<SubscriptionIntermediateFuture<String>> subscribers = new ArrayList<>();
 
     @Override
     public ISubscriptionIntermediateFuture<String> subscribeComs() {
@@ -23,7 +26,7 @@ public class ComsService implements IComsService {
     @Override
     public void broadcast(String msg) {
         for(SubscriptionIntermediateFuture<String> subscriber: subscribers){
-            subscriber.addIntermediateResultIfUndone(msg);
+            subscriber.addIntermediateResultIfUndone(msg.toString());
         }
     }
 }
