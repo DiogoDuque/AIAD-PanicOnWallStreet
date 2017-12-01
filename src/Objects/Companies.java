a501ebb35dacf3fbeb77085ed6fb419c15785eb4
Bruuuuno <pandasarehere@gmail.com>
2017-12-01 06:33:20 +0000

    Update Companies.java

--- src/Objects/Companies.java
+++ src/Objects/Companies.java
@@ -10,7 +10,16 @@ public class Companies {
     private int numShares;
     private double compValue;
 
-    public String status;
+    public boolean status;
 
     private List<Shares> shares;
+    
+    	public Company (String name , int numShares){
+		this.name = name;
+		this.numShares = numShares;
+		shares = new List<Share>(numShares);
+		
+	}
+    
+    
 }
