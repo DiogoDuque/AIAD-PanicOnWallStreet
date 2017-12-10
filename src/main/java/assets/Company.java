76b63ae749baa93f7a0323c3fa98cef4d47a0ec3
Bruno <pandasarehere@gmail.com>
2017-12-10 23:14:14 +0000

    kind of a merge

--- src/main/java/assets/Company.java
+++ src/main/java/assets/Company.java
@@ -38,11 +38,35 @@ public class Company {
     }
 
     int maxValue(){
-        return values[currIndex] + this.diceValues[this.diceValues.length - 1];
+        int maxIndexValue;
+        int maxDiceValue = this.diceValues[diceValues.length - 1];
+
+        if(this.currIndex + maxDiceValue > this.values.length){
+            maxIndexValue = this.values.length - 1;
+        } else if (this.currIndex + maxDiceValue < 0){
+            maxIndexValue = 0;
+        }
+        else{
+            maxIndexValue = this.currIndex + maxDiceValue;
+        }
+
+        return  this.values[maxIndexValue];
     }
 
     int minValue(){
-        return values[currIndex] + this.diceValues[0];
+        int minIndexValue;
+        int minDiceValue = this.diceValues[0];
+
+        if(this.currIndex + minDiceValue > this.values.length){
+            minIndexValue = this.values.length - 1;
+        } else if (this.currIndex + minDiceValue < 0){
+            minIndexValue = 0;
+        }
+        else{
+            minIndexValue = this.currIndex + minDiceValue;
+        }
+
+        return  this.values[minIndexValue];
     }
 
     /**
@@ -66,6 +90,21 @@ public class Company {
 
         return (float)(averageValue/6);
     }
+
+
+    public int risk() {
+        int riskLevel;
+        if (this.diceValues[0] == -1) {
+            riskLevel = 0;
+        } else if (this.diceValues[0] == -2) {
+            riskLevel = 1;
+        } else if (this.diceValues[0] == -3) {
+            riskLevel = 2;
+        } else riskLevel = 3;
+        return riskLevel;
+    }
+
+    ;
 
     /**
      * Rolls the dice for this company, making its value change.
